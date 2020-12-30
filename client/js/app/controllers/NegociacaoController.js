375f293f02bb27e2d1ee7a48762befce6e7f5460
luccasadriano <luccas-adriano@hotmail.com>
2020-12-30 17:30:10 -0300

    03 ligação entre ações

---
+++ client/js/app/controllers/NegociacaoController.js
@@ -0,0 +1,25 @@
+class NegociacaoController {
+   constructor() {
+
+      let $ = document.querySelector.bind(document)
+
+      this._inputQuantidade = $('#quantidade')
+      this._inputData = $('#data')
+      this._inputValor = $('#valor')
+   }
+
+   adiciona(event) {
+      event.preventDefault()
+
+      console.log(typeof(this._inputData.value))
+
+      console.log(this._inputData.value)
+
+
+      // let negociacao = new Negociacao(this._inputData.value, 
+      //    this._inputQuantidade.value, 
+      //    this._inputValor.value)
+
+      console.log(negociacao)
+   }
+}
