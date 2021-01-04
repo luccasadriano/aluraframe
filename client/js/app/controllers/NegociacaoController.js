4edbe01dfa4ccea0987d247515abfe5275e4d24a
luccasadriano <luccas-adriano@hotmail.com>
2021-01-04 16:42:23 -0300

    finalizado 03 a ligação entre as ações do usuário e o modelo

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -11,14 +11,13 @@ class NegociacaoController {
    adiciona(event) {
       event.preventDefault()
 
-      console.log(typeof(this._inputData.value))
+      let data = new Date(...this._inputData.value.split('-')
+         .map((item, indice) => item - indice % 2)
+      )
 
-      console.log(this._inputData.value)
-
-
-      // let negociacao = new Negociacao(this._inputData.value, 
-      //    this._inputQuantidade.value, 
-      //    this._inputValor.value)
+      let negociacao = new Negociacao(data, 
+         this._inputQuantidade.value, 
+         this._inputValor.value)
 
       console.log(negociacao)
    }
